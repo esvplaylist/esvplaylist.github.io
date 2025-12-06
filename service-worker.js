bdab6c8d849493bb3827abcb727267369cfcb00c
esvplaylist <janpetri90@duck.com>
2025-12-06 14:49:54 -0600

    new version

--- service-worker.js
+++ service-worker.js
@@ -8,12 +8,12 @@ var APP_PREFIX = 'esvp_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_12';
+var VERSION = 'version_13';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/music.png`
+  `${GHPATH}/headphone-symbol.png`
 ]
