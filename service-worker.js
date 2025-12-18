9bbfaeda99ea2a79df0c403ed2b66bf762d421aa
esvplaylist <janpetri90@duck.com>
2025-12-18 10:03:35 -0600

    offline mode

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ var APP_PREFIX = 'esvp_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_43';
+var VERSION = 'version_44';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -17,3 +17,21 @@ var URLS = [
   `${GHPATH}/index.html`,
   `${GHPATH}/headphone-symbol.png`
 ]
+
+const CACHE = 'esv-playlist-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('fetch', e => {
+    if (e.request.url.includes('audio.esv.org')) {
+        e.respondWith(
+            caches.open(CACHE).then(async cache => {
+                const cached = await cache.match(e.request);
+                if (cached) return cached;
+                const res = await fetch(e.request);
+                cache.put(e.request, res.clone());
+                return res;
+            })
+        );
+    }
+});
